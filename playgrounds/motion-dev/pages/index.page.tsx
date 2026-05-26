52e5c0f9999fa2e13a0c4e5956e9e44e906c9528
Nurul Akbar Al-Ghifari <nurulakbaral@gmail.com>
2026-05-26 21:14:30 +0700

    Feat(Motion Dev): Exit Animation

--- playgrounds/motion-dev/pages/index.page.tsx
+++ playgrounds/motion-dev/pages/index.page.tsx
@@ -1,5 +1,34 @@
-import { motion } from "motion/react";
+import { AnimatePresence, motion } from "motion/react";
 import * as React from "react";
+
+function ExitAnimation() {
+	const [isEntering, setIsEntering] = React.useState(true);
+
+	return (
+		<div className="text-center">
+			<AnimatePresence>
+				{isEntering && (
+					<motion.div
+						key="exit-mode"
+						exit={{ scale: 0 }}
+						initial={{ scale: 0 }}
+						animate={{ scale: 1 }}
+						transition={{ duration: 0.75 }}
+						className="mx-auto w-20 h-20 bg-green-400 mt-20"
+					/>
+				)}
+			</AnimatePresence>
+
+			<button
+				className="border-gray-200 bg-gray-200 p-2 rounded-2xl"
+				onClick={() => setIsEntering(!isEntering)}
+				type="button"
+			>
+				Toggle
+			</button>
+		</div>
+	);
+}
 
 function EnterAnimation() {
 	const [isEntering, setIsEntering] = React.useState(true);
@@ -24,6 +53,7 @@ export default function App() {
 	return (
 		<div>
 			<EnterAnimation />
+			<ExitAnimation />
 		</div>
 	);
 }
