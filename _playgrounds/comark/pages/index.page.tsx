a42c62d23d891c468decd6986e2a7e26783cd6e5
Nurul Akbar Al-Ghifari <nurulakbaral@gmail.com>
2026-09-12 16:33:09 +0700

    Feat(Comark): Async Parsing

--- _playgrounds/comark/pages/index.page.tsx
+++ _playgrounds/comark/pages/index.page.tsx
@@ -1,7 +1,35 @@
+import { MarkdownDocument } from "@comark/react";
+import { createMarkdownParser, type MarkdownDocument as TMarkdownDocument } from "comark";
+import * as React from "react";
+
+const content = `
+
+# Hello World
+
+This is **markdown** with _Comark components_.
+
+- [ ] Hello
+- [ ] World!
+`;
+
+const parser = createMarkdownParser();
+
 export default function App() {
+	const [document, setDocument] = React.useState<TMarkdownDocument | undefined>(undefined);
+
+	React.useEffect(() => {
+		void (async function process() {
+			const parsed = await parser(content, {
+				streaming: true,
+			});
+
+			setDocument(parsed);
+		})();
+	}, []);
+
 	return (
-		<div>
-			<h1 className="text-center mt-20 text-4xl">Hello World!</h1>
+		<div className="p-20">
+			<MarkdownDocument value={document} />
 		</div>
 	);
 }
